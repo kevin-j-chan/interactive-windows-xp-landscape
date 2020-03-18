7c2fa16a5114534c66756276172318edba11b082
aloop99 <35413037+aloop99@users.noreply.github.com>
2020-03-17 17:50:05 -0700

    Add files via upload

---
+++ Curve.h
@@ -0,0 +1,32 @@
+#ifndef _CURVE_H_
+#define _CURVE_H_
+
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>
+#else
+#include <GL/glew.h>
+#endif
+
+#include <glm/glm.hpp>
+#include <vector>
+
+class Curve
+{
+private:
+	std::vector<glm::vec3> points;
+	std::vector<glm::vec3> normals;
+	std::vector<float> sNum, edge;
+
+
+	std::vector<Curve*> curves;
+	GLuint curveVao, curveVbo, normalVbo, otherVbo, otherVbo2;
+	GLuint curveShader;
+public:
+	Curve();
+	void draw(glm::mat4 C);
+	void update();
+	void addPoints(std::vector<glm::vec3> p);
+};
+
+#endif
+
